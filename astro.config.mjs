7fe93e3b54223c5a617218140ade301f86a8f098
Miguel Terán <miguel.teranj02@gmail.com>
2026-01-29 06:00:53 -0400

    Enhance project configuration and structure by adding Tailwind CSS and Astro Icon integrations, updating dependencies, and refining TypeScript settings. Updated the main page layout to include a new maintenance component.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,23 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import tailwindcss from '@tailwindcss/vite';
+
+import icon from 'astro-icon';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()]
+  },
+
+  devToolbar: {
+    enabled: false,
+  },
+
+  integrations: [icon({
+    include: {
+      pixel: ["code-block-solid"]
+    }
+  })]
+});
